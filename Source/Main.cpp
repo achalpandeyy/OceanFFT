23062853b557ac295d8776388a9f32897c09fe9f
achalpandeyy <pandeyachal.ap@gmail.com>
2021-01-22 23:27:26 +0530

    Compute twiddle factors

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,6 +6,7 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/integer.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 #include <vector>
@@ -26,6 +27,8 @@ struct OceanFFT final : public Ogle::Application
 
     void Initialize() override
     {
+        // Generate grid
+
         // For each X & Z
         const int grid_dim = 1024;
         const int vertex_count = grid_dim + 1;
@@ -122,17 +125,12 @@ struct OceanFFT final : public Ogle::Application
 
         tilde_h0_program->SetInt("s_Noise3", 5);
         noise3->Bind(5);
-        
-        glClearColor(0.f, 0.f, 0.f, 1.f);
 
-        unsigned int work_group_count = DISPLACEMENT_MAP_DIM / 32;
-        glDispatchCompute(work_group_count, work_group_count, 1);
-
+        glDispatchCompute(32, 32, 1);
         glFinish();
 
         tilde_h_k_t_program = std::make_unique<Ogle::Shader>("C:/Projects/OceanFFT/Source/Shaders/TildeHKT.comp");
 
-        // Todo: This all shouldn't be in a loop, right?
         tilde_h_k_t_program->Bind();
         tilde_h0_k->BindImage(0, GL_READ_ONLY, GL_RG32F);
         tilde_h0_minus_k->BindImage(1, GL_READ_ONLY, GL_RG32F);
@@ -145,6 +143,48 @@ struct OceanFFT final : public Ogle::Application
         
         tilde_h_k_t_dz = std::make_unique<Ogle::Texture2D>(DISPLACEMENT_MAP_DIM, DISPLACEMENT_MAP_DIM, GL_RGBA32F, GL_RGBA, GL_FLOAT);
         tilde_h_k_t_dz->BindImage(4, GL_WRITE_ONLY, GL_RGBA32F);
+
+        // Generate Twiddle Factors
+        const unsigned int bit_reversed_indices[] =
+        {
+            0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0,
+            0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8,
+            0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4,
+            0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC,
+            0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2,
+            0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+            0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6,
+            0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+            0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+            0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9,
+            0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+            0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+            0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3,
+            0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+            0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7,
+            0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
+        };
+
+        twiddle_factors = std::make_unique<Ogle::Texture2D>(glm::log2(DISPLACEMENT_MAP_DIM), DISPLACEMENT_MAP_DIM, GL_RGBA32F, GL_RGBA,
+            GL_FLOAT);
+
+        twiddle_factors_program = std::make_unique<Ogle::Shader>("C:/Projects/OceanFFT/Source/Shaders/TwiddleFactors.comp");
+        twiddle_factors_program->Bind();
+
+        // Todo: OpenGL spec says, at the very least, UBOs will be no slower than SSBOs, it would be nice to
+        // make this into a UBO and enjoy whatever perf gain I can
+
+        Ogle::ShaderStorageBuffer ssbo(bit_reversed_indices, sizeof(bit_reversed_indices));
+        ssbo.Bind();
+        ssbo.BindBase(0);
+        ssbo.Unbind();
+        
+        // Todo: I can't use a binding point of 0, if I do it disturbs tilde_h_k_t_d* class of textures as I can
+        // see as artifacts when I visualize them, how tf is this happening?
+        twiddle_factors->BindImage(6, GL_WRITE_ONLY, GL_RGBA32F);
+        
+        glDispatchCompute(8, 32, 1);
+        glFinish();
     }
 
     void Update() override
@@ -161,15 +201,15 @@ struct OceanFFT final : public Ogle::Application
         tilde_h_k_t_program->Bind();
         tilde_h_k_t_program->SetFloat("u_Time", (float)glfwGetTime());
         
-        unsigned int work_group_count = DISPLACEMENT_MAP_DIM / COMPUTE_WORK_GROUP_DIM;
-        glDispatchCompute(work_group_count, work_group_count, 1);
+        glDispatchCompute(32, 32, 1);
         
         glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 
         quad_shader->Bind();
 
         quad_shader->SetInt("s_Texture", 0);
-        tilde_h_k_t_dy->Bind(0);
+        // tilde_h_k_t_dy->Bind(0);
+        twiddle_factors->Bind(0);
 
         quad_ibo->Bind();
         quad_vao->Bind();
@@ -210,8 +250,12 @@ private:
     std::unique_ptr<Ogle::Texture2D> tilde_h_k_t_dy = nullptr;
     std::unique_ptr<Ogle::Texture2D> tilde_h_k_t_dz = nullptr;
 
+    std::unique_ptr<Ogle::Texture2D> twiddle_factors = nullptr;
+
     std::unique_ptr<Ogle::Shader> tilde_h0_program = nullptr;
     std::unique_ptr<Ogle::Shader> tilde_h_k_t_program = nullptr;
+
+    std::unique_ptr<Ogle::Shader> twiddle_factors_program = nullptr;
 
     std::unique_ptr<Ogle::VertexBuffer> quad_vbo = nullptr;
     std::unique_ptr<Ogle::IndexBuffer> quad_ibo = nullptr;
